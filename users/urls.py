551a79dfdaf6f8989373faca14984b2455b73f63
Xiang Chaozhu <XiangCZ929@gmail.com>
2018-01-04 00:28:43 +0800

    2018 1 4

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,6 @@ app_name = 'users'
 urlpatterns = [
     path(r'users/login/', login, {'template_name': 'users/login.html'},
          name = 'login'),
+    path(r'logout/', views.logout_view, name = 'logout'),
+    path(r'register/', views.register, name = 'register'),
 ]
